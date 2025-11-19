539b2b71434e65005d7824b31a3686ee901f79d5
Munendrasengar <aaryansengar5@gmail.com>
2025-11-19 17:42:06 +0530

    new update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-const allowedOrigins = (process.env.FRONTEND_URLS || process.env.FRONTEND_URL || 'http://localhost:8080')
+const allowedOrigins = ("https://quiz-master-hub-74-c2pme1llr-munendras-projects-4549e1b9.vercel.app/")
   .split(',')
   .map((origin) => origin.trim());
 
